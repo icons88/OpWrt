d8d9d3fdf7aa8ce7fc3931c813505e979ddd03af
OmyK <31337777+icons88@users.noreply.github.com>
2025-09-24 14:10:24 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
